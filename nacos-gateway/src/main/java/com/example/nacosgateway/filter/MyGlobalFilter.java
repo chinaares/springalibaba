66666cc657bf01ca9b80d9f15b11c7c2b4080b77
wuhao <wuhao.cosmo@haier.com>
2021-07-05 14:15:16 +0800

    gateway功能添加

---
+++ nacos-gateway/src/main/java/com/example/nacosgateway/filter/MyGlobalFilter.java
@@ -0,0 +1,31 @@
+package com.example.nacosgateway.filter;
+
+import cn.hutool.core.util.StrUtil;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+//这个GlobalFilter从名字中就可以看出，是一个全局过滤器，也就是说实现这个接口后，所有的请求都会被过滤，我们就不需要在去找往某个路由中加过滤器了。
+@Component
+public class MyGlobalFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("-----------------全局过滤器MyGlobalFilter---------------------\n");
+        String token = exchange.getRequest().getQueryParams().getFirst("token");
+        if (StrUtil.isBlank(token)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    //设置过滤器的优先级，值越小优先级越高
+    @Override
+    public int getOrder() {
+        return -99;
+    }
+}
