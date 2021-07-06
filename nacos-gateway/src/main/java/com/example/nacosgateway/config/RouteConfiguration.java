3e2f38b9bea8ffcd46645159b255345d8c3dcdb4
wuhao <wuhao.cosmo@haier.com>
2021-07-06 15:50:22 +0800

    gateway添加过滤器功能

---
+++ nacos-gateway/src/main/java/com/example/nacosgateway/config/RouteConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.nacosgateway.config;
+
+import com.example.nacosgateway.filter.TokenGatewayFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+class RouteConfiguration {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        log.info("ServerGatewayFilter filtet........");
+        return builder.routes()
+                .route(r -> r.path("/demo/**")
+                        .filters(f -> f.stripPrefix(1).filters(new TokenGatewayFilter()))
+                        .uri("lb://cloud-discovery-server"))
+                .build();
+    }
+
+}
