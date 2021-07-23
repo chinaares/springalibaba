91eb4c52d5a1906709dd38b1360ef054cab1abcc
wuhao <wuhao.cosmo@haier.com>
2021-07-23 15:15:18 +0800

    sql提交

--- scheduled/initsql/scheduled.sql
+++ scheduled/initsql/scheduled.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50720
  File Encoding         : 65001
 
- Date: 23/07/2021 10:10:17
+ Date: 23/07/2021 15:13:28
 */
 
 SET NAMES utf8mb4;
@@ -22,17 +22,21 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `scheduled`;
 CREATE TABLE `scheduled`  (
-  `id` int(11) NOT NULL COMMENT '主键',
-  `job_id` int(10) NOT NULL COMMENT '任务ID',
+  `job_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '任务ID',
   `bean_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'bean名称',
   `method_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '方法名称',
   `method_params` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '方法参数',
   `cron_expression` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'cron表达式',
   `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
   `job_status` tinyint(3) NOT NULL DEFAULT 1 COMMENT '状态（1正常 0暂停）',
-  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`job_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of scheduled
+-- ----------------------------
+INSERT INTO `scheduled` VALUES (1, 'demoTask', 'taskWithParams', '我是参数', '0 0/2 * * * ?', '带参数示例定时任务', 1, '2021-07-23 13:42:54', NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
