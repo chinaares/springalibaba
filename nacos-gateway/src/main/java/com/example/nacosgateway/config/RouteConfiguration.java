3c7985aa5907753258222afcba1b9e15d3f774d4
wuhao <wuhao.cosmo@haier.com>
2021-08-02 16:28:46 +0800

    修改

--- nacos-gateway/src/main/java/com/example/nacosgateway/config/RouteConfiguration.java
+++ nacos-gateway/src/main/java/com/example/nacosgateway/config/RouteConfiguration.java
@@ -1,24 +1,24 @@
 package com.example.nacosgateway.config;
 
-import com.example.nacosgateway.filter.TokenGatewayFilter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @Configuration
 @Slf4j
 class RouteConfiguration {
 
+    /**
+     * 自定义限流标志的key，多个维度可以从这里入手
+     * exchange对象中获取服务ID、请求信息，用户信息等
+     */
     @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-        log.info("ServerGatewayFilter filtet........");
-        return builder.routes()
-                .route(r -> r.path("/demo/**")
-                        .filters(f -> f.stripPrefix(1).filters(new TokenGatewayFilter()))
-                        .uri("lb://cloud-discovery-server"))
-                .build();
+    KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getHostName());
     }
 
 }
