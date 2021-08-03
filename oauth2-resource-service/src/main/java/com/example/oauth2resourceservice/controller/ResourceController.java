38172d0cf412dd5dd8254ebcc8d82a37bc6065ac
wuhao <wuhao.cosmo@haier.com>
2021-08-03 14:09:28 +0800

    oauth2四种认证模式

---
+++ oauth2-resource-service/src/main/java/com/example/oauth2resourceservice/controller/ResourceController.java
@@ -0,0 +1,17 @@
+package com.example.oauth2resourceservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourceController {
+
+    @Value("${server.port}")
+    private Integer serverPort;
+
+    @GetMapping("/port")
+    public String getPort() {
+        return "server port is :" + serverPort;
+    }
+}
