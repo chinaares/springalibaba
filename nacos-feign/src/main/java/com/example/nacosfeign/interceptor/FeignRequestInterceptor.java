7aad07c90ea7a1882bf8db42ae0abd142bc22a0a
wuhao <wuhao.cosmo@haier.com>
2021-08-04 14:14:39 +0800

    微服务之间feign调用请求头丢失的问题

---
+++ nacos-feign/src/main/java/com/example/nacosfeign/interceptor/FeignRequestInterceptor.java
@@ -0,0 +1,69 @@
+package com.example.nacosfeign.interceptor;
+
+/**
+ * @author wuhao
+ * @desc ...
+ * @date 2021-08-04 14:01:50
+ */
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Description:
+ * 微服务之间feign调用请求头丢失的问题
+ *
+ */
+@Configuration
+@Slf4j
+public class FeignRequestInterceptor implements RequestInterceptor {
+    @Override
+    public void apply(RequestTemplate template) {
+        HttpServletRequest httpServletRequest = getHttpServletRequest();
+        if (httpServletRequest != null) {
+            Map<String, String> headers = getHeaders(httpServletRequest);
+            // 传递所有请求头,防止部分丢失
+            //此处也可以只传递认证的header
+            //requestTemplate.header("Authorization", request.getHeader("Authorization"));
+            for (Map.Entry<String, String> entry : headers.entrySet()) {
+                template.header(entry.getKey(), entry.getValue());
+            }
+            log.debug("FeignRequestInterceptor:{}", template.toString());
+        }
+    }
+
+
+    private HttpServletRequest getHttpServletRequest() {
+        try {
+            return ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * 获取原请求头
+     */
+    private Map<String, String> getHeaders(HttpServletRequest request) {
+        Map<String, String> map = new LinkedHashMap<>();
+        Enumeration<String> enumeration = request.getHeaderNames();
+        if (enumeration != null) {
+            while (enumeration.hasMoreElements()) {
+                String key = enumeration.nextElement();
+                String value = request.getHeader(key);
+                map.put(key, value);
+            }
+        }
+        return map;
+    }
+}
